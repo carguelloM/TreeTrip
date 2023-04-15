dcfa30e4fe95bd99c2fda3058b7b1aacc05678e0
CarguelloM <cnarguellomartinez@gmail.com>
2023-04-15 19:44:18 -0400

    car makes

--- Hackathon/Hackathon/ContentView.swift
+++ Hackathon/Hackathon/ContentView.swift
@@ -16,6 +16,10 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear()
+        {
+            getJSON()
+        }
     }
 }
 
---
+++ Hackathon/Hackathon/car.swift
@@ -0,0 +1,109 @@
+//
+//  car.swift
+//  Hackathon
+//
+//  Created by Cesar Arguello on 4/15/23.
+//
+
+import Foundation
+import UIKit
+
+
+//func getJSON()
+//{
+//    struct Make: Codable {
+//        let make_id: String
+//        let make_display: String
+//        let make_is_common: String
+//        let make_country: String
+//    }
+//
+//    struct MakesResponse: Codable {
+//        let Makes: [Make]
+//    }
+//
+//    guard let url = URL(string: "https://www.carqueryapi.com/api/0.3/?cmd=getMakes&sold_in_us=1") else {
+//        print("Invalid URL")
+//        return
+//    }
+//
+//    URLSession.shared.dataTask(with: url) { (data, response, error) in
+//        guard let data = data else {
+//            if let error = error {
+//                print("Error: \(error.localizedDescription)")
+//            } else {
+//                print("Error: No data returned.")
+//            }
+//            return
+//        }
+//
+//        guard let httpResponse = response as? HTTPURLResponse else {
+//            print("Error: Invalid response.")
+//            return
+//        }
+//
+//        if httpResponse.statusCode != 200 {
+//            print("Error: Invalid status code \(httpResponse.statusCode).")
+//            return
+//        }
+//
+//        do {
+//            let decoder = JSONDecoder()
+//            let makesResponse = try decoder.decode(MakesResponse.self, from: data)
+//            let makes = makesResponse.Makes
+//            var makeDisplayStrings = [String]()
+//            for make in makes {
+//                      print(make.make_display)
+//                    makeDisplayStrings.append(makeDisplay)
+//                  }
+//        } catch let error {
+//            print("Error decoding JSON: \(error.localizedDescription)")
+//        }
+//    }.resume()
+//}
+
+func getMakes(from urlString: String, completion: @escaping ([String]?, Error?) -> Void) {
+    struct Make: Codable {
+        let make_id: String
+        let make_display: String
+        let make_is_common: String
+        let make_country: String
+        }
+    
+    struct MakesResponse: Codable {
+            let Makes: [Make]
+        }
+    
+    guard let url = URL(string: urlString) else {
+        completion(nil, NSError(domain: "Invalid URL", code: -1, userInfo: nil))
+        return
+    }
+
+    URLSession.shared.dataTask(with: url) { (data, response, error) in
+        guard let data = data else {
+            completion(nil, error ?? NSError(domain: "Unknown error", code: -1, userInfo: nil))
+            return
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            let makesResponse = try decoder.decode(MakesResponse.self, from: data)
+            let makes = makesResponse.Makes
+            let makeDisplays = makes.map { $0.make_display }
+            completion(makeDisplays, nil)
+        } catch let error {
+            completion(nil, error)
+        }
+    }.resume()
+}
+
+func getJSON()
+{
+    getMakes(from: "https://www.carqueryapi.com/api/0.3/?cmd=getMakes&sold_in_us=1") { (makeDisplays, error) in
+        if let error = error {
+            print("Error decoding JSON: \(error.localizedDescription)")
+        } else {
+            print("Make displays: \(makeDisplays ?? [])")
+        }
+    }
+}
