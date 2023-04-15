f5ede07603c4f80e537762ce4109eb7e41e9651a
Hadi Debs <113387817+muffinman987@users.noreply.github.com>
2023-04-15 13:29:31 -0400

    init proj

---
+++ Hackathon/Hackathon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hackathon
+//
+//  Created by Hadi DEBS on 4/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
