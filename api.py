7c02f09f193393e3e644a096a278e6469e1c1495
Ravindra Mangar <ravindra.mangar@my.uwi.edu>
2023-04-16 05:58:55 -0400

    Added stuff

--- api.py
+++ api.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify, make_response, session
 from flask_session import Session
 from utils import *
+import datetime
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
@@ -26,6 +27,7 @@ def get_stats_test():
     response.status_code = 200
     return response
 
+
 @app.route("/start_session", methods=["POST"])
 def start_session():
 
@@ -35,27 +37,52 @@ def start_session():
     session["emissions"] = None
     session["tree"] = None
     session["amount_of_trees"] = None
+    session["in_motion"] = True
+    session["last_message_time"] = None
 
-    session["year"] = request.form.get("year")
-    session["make"] = request.form.get("make")
-    session["model"] = request.form.get("model")
+    req = request.get_json()
 
-    response = make_response()
+    session["year"] = req["year"]
+    session["make"] = req["make"]
+    session["model"] = req["model"]
+
+    print(session["year"], session["make"], session["model"])
+
+    response = make_response("Success")
     response.status_code = 200
     return response
 
 @app.route("/store_point", methods=["POST"])
 def store_point():
-    print(request.get_json())
-    lat = request.form.get("latitude")
-    long = request.form.get("longitude")
+    # if session["last_message_time"] == None:
+    #     session["last_message_time"] = request.
+    # t = request.time
+    req = request.get_json()
+    t = req["timestamp"]
+    lat = req["latitude"]
+    long = req["longitude"]
 
     print(lat, long)
 
-    session["gps_coords"].append((lat, long))
+    #do a check to see if car is still
+    try:
+        last_coords = session["gps_coords"][-1]
 
-    response = make_response()
-    response.status_code = 200
+        d = get_dist_between_two_points(last_coords, (lat, long))
+
+        if d > 0.1:
+            status_code = 200
+        else:
+            status_code = 208
+    except:
+        status_code = 200
+
+
+    session["gps_coords"].append((lat, long))
+    print(type(session["gps_coords"]), session["gps_coords"])
+
+    response = make_response("Success")
+    response.status_code = status_code
     return response
 
 @app.route("/get_distance_travelled", methods=["GET"])
@@ -131,13 +158,13 @@ def set_carbon_emissions():
                                                make=session["make"],
                                                model=session["model"],
                                                year=session["year"])
+
 def set_tree():
     end_lat, end_long = session["gps_coords"][-1]
     session["tree"] = find_closest_tree(end_lat, end_long)
 
 def set_amt_of_trees():
     session["amount_of_trees"] = calculate_num_trees(session["emissions"])
-
 
 
 if __name__ == "__main__":
